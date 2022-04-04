e6b06b2b210d5cafc508daadf5e4585e9a5a1d70
Drew-Trumbaturi <30042619@student.toiohomai.ac.nz>
2022-04-04 15:44:42 +1200

    made the nav sticky when scrolling

--- src/Compnents/Nav.js
+++ src/Compnents/Nav.js
@@ -2,31 +2,33 @@ import {Link} from 'react-router-dom';
 
 function Nav() {
     return (
-        <nav className='navbar navbar-dark bg-dark row'>
-            <div className='col-5'>
-            <Link to='/' className='text-white text-decoration-none'>
-                <div className='row'>
-                    <div className='col-sm-1'>
-                        <img src='images/Logo.png' alt='scp logo'/>
+        <nav className='navbar sticky-top navbar-dark bg-dark'>
+            <div className='row container-fluid'>
+                <div className='col-5'>
+                <Link to='/' className='text-white text-decoration-none'>
+                    <div className='row'>
+                        <div className='col-sm-1'>
+                            <img src='images/Logo.png' alt='scp logo'/>
+                        </div>
+                        <div className='col-sm-11 mt-3'>
+                            <h4 className='ms-2'>SCP Foundation</h4>
+                        </div>
                     </div>
-                    <div className='col-sm-11 mt-3'>
-                        <h4 className='ms-2'>SCP Foundation</h4>
-                    </div>
+                </Link>
                 </div>
-            </Link>
-            </div>
 
-            <div className='col-7'>
-                <div className='center'>
-                    <ul className="nav"> 
-                        <Link to='/' className='nav-item text-decoration-none'>
-                            <li className='nav-link text-white'>Home</li>
-                        </Link>
+                <div className='col-7'>
+                    <div className='center'>
+                        <ul className="nav"> 
+                            <Link to='/' className='nav-item text-decoration-none'>
+                                <li className='nav-link text-white'>Home</li>
+                            </Link>
 
-                        <Link to='/Catalog' className='nav-item text-decoration-none'>
-                            <li className='nav-link text-white'>Catalog</li>
-                        </Link>
-                    </ul>
+                            <Link to='/Catalog' className='nav-item text-decoration-none'>
+                                <li className='nav-link text-white'>Catalog</li>
+                            </Link>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </nav>
